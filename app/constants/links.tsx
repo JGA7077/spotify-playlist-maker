9dffe2fea40acf64a6f193a2af39aced7fdb56f9
João Gabriel <joaogabriel-al@hotmail.com>
2024-01-07 17:24:14 -0300

    feat: spotify redirect constant

---
+++ app/constants/links.tsx
@@ -0,0 +1,3 @@
+const SPOTIFY_REDIRECT = 'https://accounts.spotify.com/authorize?client_id=230be2f46909426b8b80cac36446b52a&scope=playlist-read-private%20playlist-read-collaborative%20playlist-modify-public%20user-read-recently-played%20playlist-modify-private%20ugc-image-upload%20user-follow-modify%20user-follow-read%20user-library-read%20user-library-modify%20user-read-private%20user-read-email%20user-top-read%20user-read-playback-state&response_type=token&redirect_uri=http://localhost:3000/callback';
+
+export { SPOTIFY_REDIRECT };
